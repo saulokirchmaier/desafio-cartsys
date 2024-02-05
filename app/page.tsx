c1001e7f03c0b1d836a4a9ec6a8386133ea247c2
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2024-02-05 17:12:12 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="w-screen h-screen relative flex">
+      <div className="flex justify-center w-full p-1 h-10">
+        <h2 className="text-xl">Wizzard Creator</h2>
+      </div>
+    </main>
+  );
+}
